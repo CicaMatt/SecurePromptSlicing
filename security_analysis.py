d685c557d1109075257e1b918183f1de920d99ea
CicaMatt <matteocicalese01@gmail.com>
2025-05-15 18:12:03 +0200

    fixed security analysis

--- security_analysis.py
+++ security_analysis.py
@@ -44,18 +44,22 @@ class SecurityAnalysis:
             print(f"Error: {r['stderr']}\n")
 
 commands = [
+    r'cd prova_c',
+    r'rm -f *.o vuln',
+    r'cd ..',
+
     # Database creation starting from code
     r'codeql database create CodeQL/Databases/python_example_db --language=python --source-root=prova_python --overwrite',
     r'codeql database create CodeQL/Databases/c_example_db --language=c --source-root=prova_c --overwrite',
     r'codeql database create CodeQL/Databases/java_example_db --language=java --source-root=prova_java --command="mvn clean compile" --overwrite',
 
     # Query download and installation for C/C++, Python and Java
-    r'codeql pack download codeql/cpp-queries',
     r'codeql pack download codeql/python-queries',
+    r'codeql pack download codeql/cpp-queries',
     r'codeql pack download codeql/java-queries',
 
     # Database analysis using downloaded query pack
-    r'codeql database analyze CodeQL/Databases/python_example_db --format=csv --output=results_codeql/results.csv codeql/python-queries --warnings=hide',
+    r'codeql database analyze CodeQL/Databases/python_example_db --format=csv --output=results_codeql/results_py.csv codeql/python-queries --warnings=hide',
     r'codeql database analyze CodeQL/Databases/c_example_db --format=csv --output=results_codeql/results_c.csv codeql/cpp-queries --warnings=hide',
     r'codeql database analyze CodeQL/Databases/java_example_db --format=csv --output=results_codeql/results_java.csv codeql/java-queries --warnings=hide'
 ]
